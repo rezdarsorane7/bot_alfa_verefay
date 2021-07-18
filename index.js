ce2ac129dd485b2480156464b343b2c7a80bfcc7
alfa11033 <67869229+rezdarsorane7@users.noreply.github.com>
2021-07-18 05:15:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,31 @@ const client = new DiscordMusicBot();
 client.build();
 
 module.exports = client; //;-;
+
+
+ client.on("guildCreate", guild => {
+  let channel = client.channels.cache.get("866140466411995176");
+  let embed = new MessageEmbed().setColor("#146DF6")
+  .setAuthor(client.user.username, client.user.avatarURL())
+  .setTitle( `✅ Join Server`)
+  .addField("🔠 **Server Name**", `${guild.name}`)
+  .addField("👑 **Server Owner**", `${guild.owner}`)
+  .addField("🆔 **Server Id**", `${guild.id}`)
+  .addField("👥 **Member Count**", `${guild.memberCount}`)
+  .setFooter(`${client.user.tag}`);
+  channel.send(embed);
+});
+
+client.on("guildDelete", guild => {
+  let channel = client.channels.cache.get("866140466411995176");
+  let embed = new MessageEmbed()
+  .setColor("#146DF6")
+  .setAuthor(client.user.username, client.user.avatarURL())
+  .setTitle( `❌ Left Server`)
+  .addField("🔠 **Server Name**", `${guild.name}`)
+  .addField("👑 **Server Owner**", `${guild.owner}`)
+  .addField("🆔 **Server Id**", `${guild.id}`)
+  .addField("👥 **Member Count**", `${guild.memberCount}`)
+  .setFooter(`${client.user.tag}`);
+  channel.send(embed);
+}); 
