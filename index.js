51afa33ce47f4d894786aa6aef997e08d98b7ce7
alfa11033 <67869229+rezdarsorane7@users.noreply.github.com>
2021-07-18 05:32:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,30 +5,5 @@ client.build();
 
 module.exports = client; //;-;
 
-client.on("guildCreate", guild => {
-  let channel = client.channels.cache.get("866140466411995176");
-  let embed = new MessageEmbed().setColor("#ee0202")
-  .setAuthor(client.user.username, client.user.avatarURL())
-  .setTitle( ✅ Join Server)
-  .addField(" Server Name", ${guild.name})
-  .addField(" Server Owner", ${guild.owner})
-  .addField(" Server Id", ${guild.id})
-  .addField(" Member Count", ${guild.memberCount})
-  .setFooter(${client.user.tag});
-  channel.send(embed);
-});
 
-client.on("guildDelete", guild => {
-  let channel = client.channels.cache.get("866140466411995176");
-  let embed = new MessageEmbed()
-  .setColor("#ee0202")
-  .setAuthor(client.user.username, client.user.avatarURL())
-  .setTitle( ❌ Left Server)
-  .addField(" Server Name", ${guild.name})
-  .addField(" Server Owner", ${guild.owner})
-  .addField(" Server Id", ${guild.id})
-  .addField(" Member Count", ${guild.memberCount})
-  .setFooter(${client.user.tag});
-  channel.send(embed);
-});
 
