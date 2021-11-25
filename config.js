c57350ea2172438da77f2c59de56ff3fc7c1d982
alfa11033 <67869229+rezdarsorane7@users.noreply.github.com>
2021-11-25 04:43:23 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   DefaultPrefix: process.env.Prefix || "=", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/XpuNszK", //Support Server Link
-  Token: process.env.Token || "", //Discord Bot Token
+  Token: process.env.Token || "ODgwOTMyNTUyNjgzMTg4Mjk2.YSleXw.sliXNnh97Nn_FtrpA9TKAJBTQwo", //Discord Bot Token
   ClientID: process.env.Discord_ClientID || "", //Discord Client ID
   ClientSecret: process.env.Discord_ClientSecret || "", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
